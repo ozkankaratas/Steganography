b33b4c2fb89df2c49f637531567a50cdbd204e8d
Ozkan Karatas <ozkanqaratas@gmail.com>
2022-05-06 17:06:32 +0300

    Add files via upload

---
+++ Steganografi/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Steganografi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.picIsleme = new System.Windows.Forms.PictureBox();
+            this.txtMesaj = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnCoz = new System.Windows.Forms.Button();
+            this.btnGizle = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnAc = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picIsleme)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.picIsleme);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.groupBox1.Location = new System.Drawing.Point(0, 31);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(889, 442);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "İşlenecek Resim";
+            // 
+            // picIsleme
+            // 
+            this.picIsleme.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picIsleme.BackColor = System.Drawing.Color.Transparent;
+            this.picIsleme.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picIsleme.Location = new System.Drawing.Point(4, 24);
+            this.picIsleme.Margin = new System.Windows.Forms.Padding(4);
+            this.picIsleme.Name = "picIsleme";
+            this.picIsleme.Size = new System.Drawing.Size(881, 414);
+            this.picIsleme.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picIsleme.TabIndex = 0;
+            this.picIsleme.TabStop = false;
+            // 
+            // txtMesaj
+            // 
+            this.txtMesaj.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMesaj.Location = new System.Drawing.Point(27, 37);
+            this.txtMesaj.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMesaj.Multiline = true;
+            this.txtMesaj.Name = "txtMesaj";
+            this.txtMesaj.Size = new System.Drawing.Size(415, 182);
+            this.txtMesaj.TabIndex = 1;
+            this.txtMesaj.TextChanged += new System.EventHandler(this.txtMesaj_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.txtMesaj);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.Control;
+            this.groupBox2.Location = new System.Drawing.Point(0, 473);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(469, 245);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Gizlenecek Mesaj";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox3.Controls.Add(this.btnCoz);
+            this.groupBox3.Controls.Add(this.btnGizle);
+            this.groupBox3.Controls.Add(this.btnKaydet);
+            this.groupBox3.Controls.Add(this.btnAc);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.Control;
+            this.groupBox3.Location = new System.Drawing.Point(509, 473);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Size = new System.Drawing.Size(380, 245);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "İşlemler";
+            // 
+            // btnCoz
+            // 
+            this.btnCoz.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnCoz.Font = new System.Drawing.Font("MingLiU-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCoz.ForeColor = System.Drawing.Color.White;
+            this.btnCoz.Location = new System.Drawing.Point(197, 134);
+            this.btnCoz.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCoz.Name = "btnCoz";
+            this.btnCoz.Size = new System.Drawing.Size(125, 65);
+            this.btnCoz.TabIndex = 3;
+            this.btnCoz.Text = "Çöz";
+            this.btnCoz.UseVisualStyleBackColor = false;
+            this.btnCoz.Click += new System.EventHandler(this.btnCoz_Click);
+            // 
+            // btnGizle
+            // 
+            this.btnGizle.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnGizle.Font = new System.Drawing.Font("MingLiU-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGizle.ForeColor = System.Drawing.Color.White;
+            this.btnGizle.Location = new System.Drawing.Point(197, 48);
+            this.btnGizle.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGizle.Name = "btnGizle";
+            this.btnGizle.Size = new System.Drawing.Size(125, 65);
+            this.btnGizle.TabIndex = 2;
+            this.btnGizle.Text = "Gizle";
+            this.btnGizle.UseVisualStyleBackColor = false;
+            this.btnGizle.Click += new System.EventHandler(this.btnGizle_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnKaydet.Font = new System.Drawing.Font("MingLiU-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKaydet.ForeColor = System.Drawing.Color.White;
+            this.btnKaydet.Location = new System.Drawing.Point(69, 134);
+            this.btnKaydet.Margin = new System.Windows.Forms.Padding(4);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(125, 65);
+            this.btnKaydet.TabIndex = 1;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = false;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnAc
+            // 
+            this.btnAc.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnAc.Font = new System.Drawing.Font("MingLiU-ExtB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAc.ForeColor = System.Drawing.Color.White;
+            this.btnAc.Location = new System.Drawing.Point(69, 48);
+            this.btnAc.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAc.Name = "btnAc";
+            this.btnAc.Size = new System.Drawing.Size(125, 65);
+            this.btnAc.TabIndex = 0;
+            this.btnAc.Text = "Aç";
+            this.btnAc.UseVisualStyleBackColor = false;
+            this.btnAc.Click += new System.EventHandler(this.btnAc_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hakkındaToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(889, 31);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hakkındaToolStripMenuItem
+            // 
+            this.hakkındaToolStripMenuItem.BackColor = System.Drawing.Color.SteelBlue;
+            this.hakkındaToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.hakkındaToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.hakkındaToolStripMenuItem.Name = "hakkındaToolStripMenuItem";
+            this.hakkındaToolStripMenuItem.Size = new System.Drawing.Size(83, 27);
+            this.hakkındaToolStripMenuItem.Text = "İletişim";
+            this.hakkındaToolStripMenuItem.Click += new System.EventHandler(this.hakkındaToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.BackColor = System.Drawing.Color.SteelBlue;
+            this.toolStripMenuItem1.ForeColor = System.Drawing.SystemColors.Control;
+            this.toolStripMenuItem1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(80, 27);
+            this.toolStripMenuItem1.Text = "Yardım";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(889, 718);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CODER";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picIsleme)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox picIsleme;
+        private System.Windows.Forms.TextBox txtMesaj;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnCoz;
+        private System.Windows.Forms.Button btnGizle;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnAc;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hakkındaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
---
+++ Steganografi/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Steganografi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Bitmap bmp = null;
+        private string cikarilanYazi = string.Empty;
+        private void btnAc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Resim Dosyaları (*.png,*.bmp,*.jpg)|*.png;*.bmp;*.jpg";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                picIsleme.Image = Image.FromFile(dialog.FileName);
+                btnGizle.Enabled = true;
+            }
+        }
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save_dialog = new SaveFileDialog();
+            save_dialog.Filter = "Png Image|*.png|Bitmap Image|*.bmp";
+            if (save_dialog.ShowDialog() == DialogResult.OK)
+            {
+                switch (save_dialog.FilterIndex)
+                {
+                    case 0:
+                        {
+                            bmp.Save(save_dialog.FileName, ImageFormat.Png);
+                        }
+                        break;
+                    case 1:
+                        {
+                            bmp.Save(save_dialog.FileName, ImageFormat.Bmp);
+                        }
+                        break;
+                }
+            }
+            txtMesaj.Clear();
+        }
+        private void btnGizle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtMesaj.Text == "")
+                {
+                    MessageBox.Show("Hiçbir şey yazmadınız");
+                }
+                else
+                {
+                    bmp = (Bitmap)picIsleme.Image;
+                    string yazi = txtMesaj.Text;
+                    bmp = StegoIslem.yaziGizle(yazi, bmp);
+                    MessageBox.Show("İşlendi. Resmi Kaydetmeyi unutmayın!");
+                    txtMesaj.Clear();
+                }
+            }
+            catch (Exception)
+            {
+                if (txtMesaj.Text == "")
+                {
+                    MessageBox.Show("Hiçbir şey yazmadınız");
+                }
+            }
+        }
+        private void btnCoz_Click(object sender, EventArgs e)
+        {
+            bmp = (Bitmap)picIsleme.Image;
+            string coz = StegoIslem.Coz(bmp);
+            txtMesaj.Text = "";
+            txtMesaj.Text = coz;
+        }
+        private void hakkındaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("-ozkanqaratas@gmail.com\n-Ozkan KARATAS");
+        }
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("-'Aç' butonu ile bilgisayarınızdan bir resim dosyası seçebilirsiniz." +
+                "\n-Textbox içine gizlemek istediğiniz metni yazdıktan sonra 'Gizle' " +
+                "butonu ile metni resme gizleyebilirsiniz." +
+                "\n-'Kaydet' butonu ile şifrelenmiş metin dosyasını kaydedebilirsiniz." +
+                "\n-'Çöz' butonu ile daha önceden şifrelenmiş bir resim dosyasını çözümleyebilirsiniz.");
+        }
+
+        private void txtMesaj_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
